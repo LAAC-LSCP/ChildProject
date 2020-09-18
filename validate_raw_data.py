2ed6b388f3fb4b71bb7a2d46f3b086a8618f4078
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-09-18 14:36:56 +0200

    validation script

---
+++ validate_raw_data.py
@@ -0,0 +1,27 @@
+from ChildProject.ChildProject import ChildProject
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--source", help = "source data path", required = True)
+args = parser.parse_args()
+
+project = ChildProject()
+project.raw_data_path = args.source
+
+try:
+    results = project.validate_input_data()
+except Exception as e:
+    print("Validation stopped with the following error:", file = sys.stderr)
+    print(str(e), file = sys.stderr)
+    sys.exit(1)
+
+for error in results['errors']:
+    print("error: {}".format(error), file = sys.stderr)
+
+for warning in results['warnings']:
+    print("warning: {}".format(warning))
+
+if len(results['errors']) > 0:
+    print("validation failed, {} error(s) occured".format(len(results['errors'])), file = sys.stderr)
+
