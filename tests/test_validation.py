14e5033ecb66006f2296c95947790987d6d793c8
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-10-28 09:54:01 +0100

    update validation tests accordingly

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -13,7 +13,8 @@ def test_invalid_project():
     
     expected_errors = [
         "child_id '1' appears 2 times in lines [2,3], should appear once",
-        "cannot find recording 'test_1_20200918.mp3'"
+        "cannot find recording 'test_1_20200918.mp3'",
+        "'USB' does not match the format required for 'recording_device_type' on line 2, expected '(lena|usb|olympus|babylogger)'"
     ]
     
     assert sorted(expected_errors) == sorted(errors), "errors do not match expected errors"
