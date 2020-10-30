a7ee0a8e4256e1f2d9da6555b5a15db564be1320
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-10-30 11:59:16 +0100

    allow to import one annotation at a time using the script
    Closes #55

--- import_annotations.py
+++ import_annotations.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from ChildProject.projects import ChildProject
 from ChildProject.annotations import AnnotationManager
 import argparse
@@ -6,7 +7,13 @@ import sys
 
 parser = argparse.ArgumentParser(description='import and convert annotations into the project')
 parser.add_argument("--source", help = "project path", required = True)
-parser.add_argument("--annotations", help = "path to input annotations index (csv)", required = True)
+parser.add_argument("--annotations", help = "path to input annotations index (csv)", default = "")
+
+for col in AnnotationManager.INDEX_COLUMNS:
+    if col.generated:
+        continue
+
+    parser.add_argument("--{}".format(col.name), help = col.description, type = str, default = None)
 
 args = parser.parse_args()
 
@@ -17,8 +24,13 @@ if len(errors) > 0:
     print("validation failed, {} error(s) occured".format(len(errors)), file = sys.stderr)
     sys.exit(1)
 
+if args.annotations:
+    annotations = pd.read_csv(args.annotations)
+else:
+    annotations = pd.DataFrame([{col.name: getattr(args, col.name) for col in AnnotationManager.INDEX_COLUMNS if not col.generated}])
+
 am = AnnotationManager(project)
-am.import_annotations(pd.read_csv(args.annotations))
+am.import_annotations(annotations)
 
 errors, warnings = am.validate()
 
