5ad07025297b67449bdecdba873ea8c32d157915
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-11-04 16:06:52 +0100

    make child-project executable while installing the pip package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ChildProject',
@@ -8,7 +8,8 @@ setup(
     author='Lucas',
     author_email='lucas.gautheron@gmail.com',
     license='unlicense',
-    packages=['ChildProject'],
+    packages=find_packages(),
     install_requires=['pandas', 'xlrd', 'jinja2', 'numpy', 'pympi-ling', 'sox'],
+    scripts=['child-project'],
     zip_safe=False
 )
