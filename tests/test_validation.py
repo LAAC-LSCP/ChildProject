74ba4eae6c9940573545d5cddd35aa08a107846e
Lucas Gautheron <lucas.gautheron@gmail.com>
2020-12-18 10:45:45 +0100

    validation test fix

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,14 +2,14 @@ from ChildProject.projects import ChildProject
 
 def test_valid_project():
     project = ChildProject("examples/valid_raw_data")
-    errors, warnings = project.validate_input_data()
+    errors, warnings = project.validate()
 
     assert len(errors) == 0, "valid input validation failed (expected to pass)"
     assert len(warnings) == 1, "expected 1 warning, got {}".format(len(warnings))
 
 def test_invalid_project():
     project = ChildProject("examples/invalid_raw_data")
-    errors, warnings = project.validate_input_data()
+    errors, warnings = project.validate()
     
     expected_errors = [
         "child_id '1' appears 2 times in lines [2,3], should appear once",
