8dc198fc84bf5401da54da1b280826cd9d917e33
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-02-26 11:41:58 +0100

    parallelized energy-based sampler

--- ChildProject/pipelines/sampler.py
+++ ChildProject/pipelines/sampler.py
@@ -115,6 +115,8 @@ class EnergyDetectionSampler(Sampler):
     :type low_freq: int, optional
     :param high_freq: if < 100000, frequencies above will be filtered before calculating the energy, defaults to 100000
     :type high_freq: int, optional
+    :param threads: amount of threads to run on, defaults to 1
+    :type threads: int, optional
     """
     def __init__(self,
         project: ChildProject.projects.ChildProject,
@@ -124,7 +126,9 @@ class EnergyDetectionSampler(Sampler):
         windows_offset: float = 0,
         threshold: float = 0.8,
         low_freq: int = 0,
-        high_freq: int = 100000
+        high_freq: int = 100000,
+        threads: int = 1,
+        profile: str = ''
         ):
 
         super().__init__(project)
@@ -135,6 +139,8 @@ class EnergyDetectionSampler(Sampler):
         self.threshold = threshold
         self.low_freq = low_freq
         self.high_freq = high_freq
+        self.threads = threads
+        self.profile = profile
 
     def compute_energy_loudness(self, chunk, sampling_frequency: int):
         if self.low_freq > 0 or self.high_freq < 100000:
@@ -145,8 +151,12 @@ class EnergyDetectionSampler(Sampler):
         else:
             return np.sum(chunk**2)
 
-    def get_recording_windows(self, profile, recording):
-        recording_path = os.path.join(self.project.path, ChildProject.projects.ChildProject.RAW_RECORDINGS, recording['filename'])
+    def get_recording_windows(self, recording):
+        if self.profile:
+            recording_path = os.path.join(self.project.path, ChildProject.projects.ChildProject.CONVERTED_RECORDINGS, self.profile, recording['filename'])
+        else:
+            recording_path = os.path.join(self.project.path, ChildProject.projects.ChildProject.RAW_RECORDINGS, recording['filename'])
+
         audio = AudioSegment.from_wav(recording_path)
         duration = audio.duration_seconds
         channels = audio.channels
@@ -172,19 +182,20 @@ class EnergyDetectionSampler(Sampler):
                 'energy': energy
             })
 
-        return windows
+        return pd.DataFrame(windows).set_index('recording_filename')
         
 
     def sample(self):
-        segments = []
-        for recording in self.project.recordings.to_dict(orient = 'records'):
-            windows = pd.DataFrame(self.get_recording_windows('', recording))
-            energy_threshold = windows['energy'].quantile(self.threshold)
-            windows = windows[windows['energy'] >= energy_threshold]
-            windows = windows.sample(self.windows_count)
-            segments.extend(windows.to_dict(orient = 'records'))
-
-        self.segments = pd.DataFrame(segments)
+        pool = mp.Pool(processes = self.threads if self.threads >= 1 else mp.cpu_count())
+        windows = pd.concat(pool.map(self.get_recording_windows, self.project.recordings.to_dict(orient = 'records')))
+        windows = windows.merge(
+            windows.groupby('recording_filename').agg(energy_threshold = ('energy', lambda a: np.quantile(a, self.threshold))),
+            left_index = True,
+            right_index = True
+        )
+        windows = windows[windows['energy'] >= windows['energy_threshold']]
+        self.segments = windows.groupby('recording_filename').sample(self.windows_count)
+        self.segments.reset_index(inplace = True)
 
     @staticmethod
     def add_parser(samplers):
@@ -196,6 +207,10 @@ class EnergyDetectionSampler(Sampler):
         parser.add_argument('--threshold', help = 'lowest energy quantile to sample from. default is 0.8 (i.e., sample from the 20%% windows with the highest energy).', default = 0.8, type = float)
         parser.add_argument('--low-freq', help = 'remove all frequencies below low-freq before calculating each window\'s energy. (in Hz)', default = 0, type = int)
         parser.add_argument('--high-freq', help = 'remove all frequencies above high-freq before calculating each window\'s energy. (in Hz)', default = 100000, type = int)
+        parser.add_argument('--threads', help = 'amount of threads to run on', default = 1, type = int)
+        parser.add_argument('--profile', help = 'name of the profile of recordings to use (uses raw recordings if empty)', default = '', type = str)
+
+
 
 class HighVolubilitySampler(Sampler):
     def __init__(self,
