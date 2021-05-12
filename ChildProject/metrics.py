7153c366c0e755b0e70e962a0a6e6779f80a7dec
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-05-12 10:28:45 +0200

    nltk agreement metrics + example

--- ChildProject/metrics.py
+++ ChildProject/metrics.py
@@ -5,41 +5,6 @@ from .annotations import AnnotationManager
 
 #from nltk.metrics.agreement import AnnotationTask
 
-def gamma(segments: pd.DataFrame, column: str, alpha = 1, beta = 1, precision_level = 0.05) -> float:
-    """compute gamma agreement on `segments`. (doi:10.1162/COLI_a_00227,https://hal.archives-ouvertes.fr/hal-03144116) 
-
-    :param segments: input segments dataframe (see :ref:`format-annotations-segments` for the dataframe format)
-    :type segments: pd.DataFrame
-    :param column: name of the categorical column of the segments to consider, e.g. 'speaker_type'
-    :type column: str
-    :param alpha: gamma agreement time alignment weight, defaults to 1
-    :type alpha: int, optional
-    :param beta: gamma agreement categorical weight, defaults to 1
-    :type beta: int, optional
-    :param precision_level: level of precision (see pygamma-agreement's documentation), defaults to 0.05
-    :type precision_level: float, optional
-    :return: gamma agreement
-    :rtype: float
-    """
-
-    from pyannote.core import Segment
-    from pygamma_agreement.continuum import Continuum
-    from pygamma_agreement.dissimilarity import CombinedCategoricalDissimilarity
-
-    continuum = Continuum()
-
-    for segment in segments.to_dict(orient = 'records'):
-        continuum.add(segment['set'], Segment(segment['segment_onset'], segment['segment_offset']), segment[column])
-
-    dissim = CombinedCategoricalDissimilarity(list(continuum.categories),
-                                            delta_empty=1,
-                                            alpha = alpha,
-                                            beta = beta)
-
-    gamma_results = continuum.compute_gamma(dissim, precision_level = precision_level)
-
-    return gamma_results.gamma
-
 def segments_to_annotation(segments: pd.DataFrame, column: str):
     """Transform a dataframe of annotation segments into a pyannote.core.Annotation object
 
@@ -147,7 +112,7 @@ def segments_to_grid(
 
     return data
 
-def grid_to_categories(grid, categories):
+def grid_to_vector(grid, categories):
     """Transform a grid of active classes into a vector of labels.
     In case several classes are active at time i, the label is 
     set to 'overlap'.
@@ -179,7 +144,61 @@ def conf_matrix(horizontal_grid, vertical_grid, categories):
     """
     from sklearn.metrics import confusion_matrix
 
-    vertical = grid_to_categories(vertical_grid, categories)
-    horizontal = grid_to_categories(horizontal_grid, categories)
+    vertical = grid_to_vector(vertical_grid, categories)
+    horizontal = grid_to_vector(horizontal_grid, categories)
 
     return confusion_matrix(vertical, horizontal, labels = categories)
+
+
+def vectors_to_annotation_task(*args):
+    """transform vectors of labels into a nltk AnnotationTask object.
+
+    :return: the AnnotationTask object
+    :rtype: nltk.metrics.agreement.AnnotationTask
+    """
+    from nltk.metrics import agreement
+
+    v = np.vstack(args)
+    it = np.nditer(v, flags = ['multi_index'])
+    data = [
+        (it.multi_index[0], it.multi_index[1], str(x))
+        for x in it
+    ]
+
+    return agreement.AnnotationTask(data = data)
+
+def gamma(segments: pd.DataFrame, column: str, alpha = 1, beta = 1, precision_level = 0.05) -> float:
+    """compute gamma agreement on `segments`. (doi:10.1162/COLI_a_00227,https://hal.archives-ouvertes.fr/hal-03144116) 
+
+    :param segments: input segments dataframe (see :ref:`format-annotations-segments` for the dataframe format)
+    :type segments: pd.DataFrame
+    :param column: name of the categorical column of the segments to consider, e.g. 'speaker_type'
+    :type column: str
+    :param alpha: gamma agreement time alignment weight, defaults to 1
+    :type alpha: int, optional
+    :param beta: gamma agreement categorical weight, defaults to 1
+    :type beta: int, optional
+    :param precision_level: level of precision (see pygamma-agreement's documentation), defaults to 0.05
+    :type precision_level: float, optional
+    :return: gamma agreement
+    :rtype: float
+    """
+
+    from pyannote.core import Segment
+    from pygamma_agreement.continuum import Continuum
+    from pygamma_agreement.dissimilarity import CombinedCategoricalDissimilarity
+
+    continuum = Continuum()
+
+    for segment in segments.to_dict(orient = 'records'):
+        continuum.add(segment['set'], Segment(segment['segment_onset'], segment['segment_offset']), segment[column])
+
+    dissim = CombinedCategoricalDissimilarity(list(continuum.categories),
+                                            delta_empty=1,
+                                            alpha = alpha,
+                                            beta = beta)
+
+    gamma_results = continuum.compute_gamma(dissim, precision_level = precision_level)
+
+    return gamma_results.gamma
+
