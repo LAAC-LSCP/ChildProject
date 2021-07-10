f477a7067787fa4fc4f77cc4db1442b02013ae13
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-07-10 18:14:53 +0200

    ACLEW pipeline

--- ChildProject/pipelines/metrics.py
+++ ChildProject/pipelines/metrics.py
@@ -72,9 +72,9 @@ class Metrics(ABC):
         
         return recordings
 
-    def retrieve_segments(self, set: str, unit: str):
+    def retrieve_segments(self, sets: List[str], unit: str):
         annotations = self.am.annotations[self.am.annotations[self.by] == unit]
-        annotations = annotations[annotations['set'] == set]
+        annotations = annotations[annotations['set'].isin(sets)]
         
         try:
             segments = self.am.get_segments(annotations)
@@ -96,7 +96,7 @@ class LenaMetrics(Metrics):
     def __init__(self,
         project: ChildProject.projects.ChildProject,
         set: str,
-        recordings: Union[str, List[str], pd.DataFrame],
+        recordings: Union[str, List[str], pd.DataFrame] = None,
         by: str = 'recording_filename',
         threads: int = 1):
 
@@ -108,7 +108,7 @@ class LenaMetrics(Metrics):
     def _process_unit(self, unit: str):
         import ast
 
-        its = self.retrieve_segments(self.set, unit)
+        its = self.retrieve_segments([self.set], unit)
         if len(its) == 0:
             return pd.DataFrame()
 
@@ -175,6 +175,141 @@ class LenaMetrics(Metrics):
     def add_parser(subparsers, subcommand):
         parser = subparsers.add_parser(subcommand, help = 'LENA metrics')
         parser.add_argument('set', help = 'name of the LENA its annotations set')
+        parser.add_argument('--threads', help = 'amount of threads to run on', default = 1, type = int)
+
+class LenaMetrics(Metrics):
+    SUBCOMMAND = 'aclew'
+
+    def __init__(self,
+        project: ChildProject.projects.ChildProject,
+        vtc: str = 'vtc',
+        alice: str = 'alice',
+        vcm: str = 'vcm',
+        recordings: Union[str, List[str], pd.DataFrame] = None,
+        by: str = 'recording_filename',
+        threads: int = 1):
+
+        super().__init__(project, by, recordings)
+
+        self.vtc = vtc
+        self.alice = alice
+        self.vcm = vcm
+        self.threads = int(threads)
+
+    def _process_unit(self, unit: str):
+        segments = self.retrieve_segments([self.vtc, self.alice, self.vcm], unit)
+
+        if len(segments) == 0:
+            return pd.DataFrame()
+
+        unit_duration = self.project.recordings[self.project.recordings[self.by] == unit]['duration'].sum()/1000
+
+        metrics = {}
+        speaker_types = ['FEM', 'MAL', 'CHI', 'OCH']
+        adults = ['FEM', 'MAL']
+
+        segments = segments[segments['speaker_type'].isin(speaker_types)]
+
+        vtc = segments[segments['set'] == self.vtc]
+        alice = segments[segments['set'] == self.alice]
+        vcm = segments[segments['set'] == self.vcm]
+
+        vtc_agg = vtc.groupby('speaker_type').agg(
+            voc_ph = ('segment_onset', lambda x: 3600*len(x)/unit_duration),
+            voc_dur_ph = ('duration', lambda x: 3600*np.sum(x)/unit_duration),
+            avg_voc_dur = ('duration', np.mean)
+        )
+
+        for speaker in speaker_types:
+            if speaker not in vtc_agg.index:
+                continue
+
+            metrics['voc_{}_ph'.format(speaker.lower())] = vtc_agg.loc[speaker, 'voc_ph']
+            metrics['voc_dur_{}_ph'.format(speaker.lower())] = vtc_agg.loc[speaker, 'voc_dur_ph']
+            metrics['avg_voc_dur_{}'.format(speaker.lower())] = vtc_agg.loc[speaker, 'avg_voc_dur']
+
+        if len(alice):
+            alice_agg = alice.groupby('speaker_type').agg(
+                wc_ph = ('words', lambda x: 3600*np.sum(x)/unit_duration),
+                sc_ph = ('syllables', lambda x: 3600*np.sum(x)/unit_duration),
+                pc_ph = ('phonemes', lambda x: 3600*np.sum(x)/unit_duration)
+            )
+
+            for speaker in adults:
+                if speaker not in alice_agg.index:
+                    continue
+            
+                metrics['wc_{}_ph'.format(speaker.lower())] = alice_agg.loc[speaker, 'wc_ph']
+                metrics['sc_{}_ph'.format(speaker.lower())] = alice_agg.loc[speaker, 'sc_ph']
+                metrics['pc_{}_ph'.format(speaker.lower())] = alice_agg.loc[speaker, 'pc_ph']
+
+            metrics['wc_adu_ph'] = alice['words'].sum()*3600/unit_duration
+            metrics['sc_adu_ph'] = alice['syllables'].sum()*3600/unit_duration
+            metrics['pc_adu_ph'] = alice['phonemes'].sum()*3600/unit_duration
+
+        if len(vcm):
+            vcm_agg = vcm[vcm['speaker_type'] == 'CHI'].groupby('vcm_type').agg(
+                voc_chi_ph = ('segment_onset', lambda x: 3600*len(x)/unit_duration),
+                voc_dur_chi_ph = ('duration', lambda x: 3600*np.sum(x)/unit_duration),
+                avg_voc_dur_chi = ('duration', np.mean)
+            )
+            
+            metrics['cry_voc_chi_ph'] = vcm_agg.loc['Y', 'voc_chi_ph'] if 'Y' in vcm_agg.index else 0
+            metrics['cry_voc_dur_chi_ph'] = vcm_agg.loc['Y', 'voc_dur_chi_ph'] if 'Y' in vcm_agg.index else 0
+
+            if 'Y' in vcm_agg.index:
+                metrics['avg_cry_voc_dur_chi'] = vcm_agg.loc['Y', 'avg_voc_dur_chi']
+
+            metrics['can_voc_chi_ph'] = vcm_agg.loc['C', 'voc_chi_ph'] if 'C' in vcm_agg.index else 0
+            metrics['can_voc_dur_chi_ph'] = vcm_agg.loc['C', 'voc_dur_chi_ph'] if 'C' in vcm_agg.index else 0
+
+            if 'C' in vcm_agg.index:
+                metrics['avg_can_voc_dur_chi'] = vcm_agg.loc['C', 'avg_voc_dur_chi']
+
+            metrics['non_can_voc_chi_ph'] = vcm_agg.loc['N', 'voc_chi_ph'] if 'N' in vcm_agg.index else 0
+            metrics['non_can_voc_dur_chi_ph'] = vcm_agg.loc['N', 'voc_dur_chi_ph'] if 'N' in vcm_agg.index else 0
+            
+            if 'N' in vcm_agg.index:        
+                metrics['avg_non_can_voc_dur_chi'] = vcm_agg.loc['N', 'avg_voc_dur_chi']
+
+            speech_voc = metrics['can_voc_chi_ph'] + metrics['non_can_voc_chi_ph']
+            speech_dur = metrics['can_voc_dur_chi_ph'] + metrics['non_can_voc_dur_chi_ph']
+
+            cry_voc = metrics['cry_voc_chi_ph']
+            cry_dur = metrics['cry_voc_dur_chi_ph']
+
+            if speech_voc+cry_voc:
+                metrics['lp_n'] = speech_voc/(speech_voc+cry_voc)
+                metrics['cp_n'] = metrics['can_voc_chi_ph']/speech_voc
+
+                metrics['lp_dur'] = speech_dur/(speech_dur+cry_dur)
+                metrics['cp_dur'] = metrics['can_voc_dur_chi_ph']/speech_dur
+
+        metrics[self.by] = unit
+        metrics['child_id'] = segments['child_id'].iloc[0]
+        metrics['duration'] = unit_duration
+
+        return pd.DataFrame([metrics])
+
+    def extract(self):
+        recordings = self.get_recordings()
+
+        if self.threads == 1:
+            self.metrics = pd.concat([self._process_unit(unit) for unit in recordings[self.by].unique()])
+        else:
+            with mp.Pool(processes = self.threads if self.threads >= 1 else mp.cpu_count()) as pool:
+                self.metrics = pd.concat(pool.map(self._process_unit, recordings[self.by].unique()))
+
+        self.metrics.set_index(self.by, inplace = True)
+        return self.metrics
+        
+
+    @staticmethod
+    def add_parser(subparsers, subcommand):
+        parser = subparsers.add_parser(subcommand, help = 'LENA metrics')
+        parser.add_argument('--vtc', help = 'vtc set', default = 'vtc')
+        parser.add_argument('--alice', help = 'alice set', default = 'alice')
+        parser.add_argument('--vcm', help = 'vcm set', default = 'vcm')
         parser.add_argument('--threads', help = 'amount of threads to run on', default = 1, type = int)
 
 class MetricsPipeline(Pipeline):
