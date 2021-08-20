0c74ba387c0f7ed46aeda6e2702c27d9fb654d23
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-08-20 14:06:49 +0200

    get_segments_timestamps

--- ChildProject/annotations.py
+++ ChildProject/annotations.py
@@ -1023,9 +1023,10 @@ class AnnotationManager:
         :param end: offset HH:MM clocktime
         :type end: str
         :return: a DataFrame of annotations;
-        For each row, ``range_onset`` and ``range_offset`` are clipped within the corresponding clock-time range.
+        For each row, ``range_onset`` and ``range_offset`` are clipped within the desired clock-time range.
         The clock-time corresponding to the onset and offset of each annotation
         is stored in two newly created columns named ``range_onset_time`` and ``range_offset_time``.
+        If the input annotation exceeds 24 hours, one row per matching interval is returned.
         :rtype: pd.DataFrame
         """
 
@@ -1077,14 +1078,14 @@ class AnnotationManager:
 
                 ann["range_onset_time"] = str(
                     datetime.timedelta(milliseconds=segment.start % (86400 * 1000))
-                )[:-3].zfill(len('00:00'))
+                )[:-3].zfill(len("00:00"))
                 ann["range_offset_time"] = str(
                     datetime.timedelta(milliseconds=segment.stop % (86400 * 1000))
-                )[:-3].zfill(len('00:00'))
+                )[:-3].zfill(len("00:00"))
 
-                if ann['range_onset'] >= ann['range_offset']:
+                if ann["range_onset"] >= ann["range_offset"]:
                     continue
-                
+
                 matches.append(ann)
 
         if len(matches):
@@ -1095,8 +1096,46 @@ class AnnotationManager:
             columns = set(annotations.columns) - {"range_onset_ts", "range_offset_ts"}
             return pd.DataFrame(columns=columns)
 
+    def get_segments_timestamps(self, segments: pd.DataFrame) -> pd.DataFrame:
+        """Calculate the onset and offset clock-time of each segment
+
+        :param segments: DataFrame of segments (as returned by :meth:`~ChildProject.annotations.AnnotationManager.get_segments`).
+        :type segments: pd.DataFrame
+        :return: Returns the input dataframe with two new columns ``onset_time`` and ``offset_time``.
+        ``onset_time`` is a datetime object corresponding to the onset of the segment.
+        ``offset_time`` is a datetime object corresponding to the offset of the segment.
+        In case either ``start_time`` or ``date_iso`` is not specified for the corresponding recording,
+        both values will be set to NaT.
+
+        :rtype: pd.DataFrame
+        """
+        columns_to_drop = set(segments.columns) & {"date_iso", "start_time"}
+
+        if len(columns_to_drop):
+            segments.drop(columns=columns_to_drop, inplace=True)
+
+        segments = segments.merge(
+            self.project.recordings[["recording_filename", "date_iso", "start_time"]],
+            how="left",
+        )
+
+        segments["start_time"] = pd.to_datetime(
+            segments[["date_iso", "start_time"]].apply(
+                lambda row: "{} {}".format(str(row["date_iso"]), str(row["start_time"])), axis=1
+            ),
+            format="%Y-%m-%d %H:%M",
+            errors="coerce",
+        )
+        segments["onset_time"] = segments["start_time"] + pd.to_timedelta(
+            segments["segment_onset"], unit="ms", errors="coerce"
+        )
+        segments["offset_time"] = segments["start_time"] + pd.to_timedelta(
+            segments["segment_offset"], unit="ms", errors="coerce"
+        )
+        return segments.drop(columns=["start_time", "date_iso"])
+
     @staticmethod
-    def intersection(annotations: pd.DataFrame, sets: list = None) -> tuple:
+    def intersection(annotations: pd.DataFrame, sets: list = None) -> pd.DataFrame:
         """Compute the intersection of all annotations for all sets and recordings,
         based on their ``recording_filename``, ``range_onset`` and ``range_offset``
         attributes. (Only these columns are required, but more can be passed and they
@@ -1105,7 +1144,7 @@ class AnnotationManager:
         :param annotations: dataframe of annotations, according to :ref:`format-annotations`
         :type annotations: pd.DataFrame
         :return: dataframe of annotations, according to :ref:`format-annotations`
-        :rtype: tuple
+        :rtype: pd.DataFrame
         """
         stack = []
         recordings = list(annotations["recording_filename"].unique())
