b80ff72ae90afc9cb4de04f928b3e55928b84cf2
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-08-20 14:27:30 +0200

    get_within_time_range doc improvement

--- ChildProject/annotations.py
+++ ChildProject/annotations.py
@@ -1016,7 +1016,8 @@ class AnnotationManager:
         """Clip all input annotations within a given HH:MM clock-time range.
         Those that do not intersect the input time range at all are filtered out.
 
-        :param annotations: DataFrame of input annotations to filter
+        :param annotations: DataFrame of input annotations to filter.
+        The only columns that are required are: ``recording_filename``, ``range_onset``, and ``range_offset``.
         :type annotations: pd.DataFrame
         :param start: onset HH:MM clocktime
         :type start: str
