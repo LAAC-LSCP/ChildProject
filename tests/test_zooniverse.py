58f90639c28839f272bd798b17b7f7c603ede350
Loann Peurey <loannpeurey@gmail.com>
2023-04-03 20:12:01 +0200

    tests for upload

--- tests/test_zooniverse.py
+++ tests/test_zooniverse.py
@@ -1,9 +1,13 @@
 import os
 import pandas as pd
 
+import pytest
+import shutil
+
 from ChildProject.projects import ChildProject
 from ChildProject.pipelines.samplers import PeriodicSampler
 from ChildProject.pipelines.zooniverse import ZooniversePipeline, pad_interval
+from ChildProject.pipelines.fake_panoptes import LOCATION_FAIL
 
 
 def test_padding():
@@ -28,6 +32,7 @@ def test_extraction():
     sampler.segments.to_csv("output/zooniverse/sampled_segments.csv")
 
     zooniverse = ZooniversePipeline()
+    
 
     chunks, parameters = zooniverse.extract_chunks(
         path=project.path,
@@ -58,3 +63,53 @@ def test_extraction():
         .tolist()
     )
 
+BASE_CHUNKS = os.path.join('tests', 'data', 'chunks_test.csv')
+# the test is done on a fake Panoptes package to avoid pushing real stuff to zooniverse
+# but the real panoptes package may change and produce other errors
+@pytest.mark.parametrize("location_fail,amount,ignore_errors,record_orphan,result", 
+                         [(False,None,False,False,'stale_obj_stop.csv'),
+                         (True,30,False,False,'loc_invalid_stop.csv'),
+                         (False,None,True,True,'stale_obj_continue_orphan.csv'),
+                         (False,30,True,False,'no_errors.csv'),
+                         (True,30,True,True,'loc_invalid_continue.csv'),
+                         ])
+def test_uploading(location_fail, amount, ignore_errors, record_orphan, result):
+    new_path = os.path.join('output', 'zooniverse', 'chunks_test.csv')
+    os.makedirs("output/zooniverse", exist_ok=True)
+    
+    df = pd.read_csv(BASE_CHUNKS)
+    
+    zooniverse = ZooniversePipeline()
+    
+    if amount is None: amount = 1000
+    
+    if not location_fail: df = df[df['mp3'] != LOCATION_FAIL]
+    
+    df.to_csv(new_path,index=False)
+    
+    zooniverse.upload_chunks(new_path,
+                             404,
+                             'test_subject_set',
+                             'test_username',
+                             'test_password',
+                             amount,
+                             ignore_errors,
+                             record_orphan,
+                             test_endpoint=True,
+                             )
+    
+    truth = pd.read_csv(os.path.join('tests','truth','zoochunks',result))
+    #shutil.copy(new_path, os.path.join('tests','truth','zoochunks',result))
+    
+    pd.testing.assert_frame_equal(truth, pd.read_csv(new_path), check_like=True)
+        
+    
+    
+def test_link_orphan():
+    pass
+    
+def test_reset_orphan():
+    pass
+    
+def test_classification():
+    pass
