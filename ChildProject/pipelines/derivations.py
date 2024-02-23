ddbe0c735170d7e28b8c1eef902ffc41458141a2
Agata Kozioł <agata.koziol.98@gmail.com>
2024-02-23 13:58:25 +0100

    Update derivations.py

--- ChildProject/pipelines/derivations.py
+++ ChildProject/pipelines/derivations.py
@@ -11,30 +11,21 @@ def conversations(annotations: pd.DataFrame,
         max_interval=1000,
         min_delay=0):
 
-    """import and convert ``annotation``. This function should not be called outside of this class.
-
-    :param import_function: If callable, ``import_function`` will be called to convert the input annotation into a dataframe. Otherwise, the conversion will be performed by a built-in function.
-    :type import_function: Callable[[str], pd.DataFrame]
-    :param output_set: name of the new set of derived annotations
-    :type output_set: str
-    :param params: Optional parameters. With ```new_tiers```, the corresponding EAF tiers will be imported
-    :type params: dict
-    :param annotation: input annotation dictionary (attributes defined according to :ref:`ChildProject.annotations.AnnotationManager.SEGMENTS_COLUMNS`)
-    :type annotation: dict
-    :param overwrite_existing: choose if lines with the same set and annotation_filename should be overwritten
-    :type overwrite_existing: bool
-    :return: output annotation dictionary (attributes defined according to :ref:`ChildProject.annotations.AnnotationManager.SEGMENTS_COLUMNS`)
-    :rtype: dict
     """
 
-    """number of conversational turn counts based on vocalizations occurring
-    in a given interval of one another
+    :param annotations: dataframe of annotations
+    :type annotations: DataFrame
+    :param interlocutors_1: first group of interlocutors, default = ('CHI')
+    :type interlocutors_1: tuple
+    :param interlocutors_2: second group of interlocutors, default = ('FEM','MAL','OCH')
+    :type interlocutors_2: tuple
+    :param max_interval: maximum interval in ms for it to be considered a turn, default = 1000
+    :type max_interval: int
+    :param min_delay: minimum delay between somebody starting speaking
+    :type min_delay: int
 
-    keyword arguments:
-        - interlocutors_1 : first group of interlocutors, default = ['CHI']
-        - interlocutors_2 : second group of interlocutors, default = ['FEM','MAL','OCH']
-        - max_interval : maximum interval in ms for it to be considered a turn, default = 1000
-        - min_delay : minimum delay between somebody starting speaking
+    :return: output annotation dataframe
+    :rtype: DataFrame
     """
 
     speakers = set(interlocutors_1 + interlocutors_2)
@@ -73,9 +64,10 @@ def conversations(annotations: pd.DataFrame,
         diff = np.diff(annotations['is_CT'].to_list() + [0])
         annotations['diff'] = pd.Series(diff)
         annotations['conv_number'] = annotations['diff'][annotations['diff'] == 1].cumsum().astype('Int64')
-        annotations['inter_conv'] = annotations[(annotations['is_CT']) | (annotations['diff'])][
+        annotations['conv_count'] = annotations[(annotations['is_CT']) | (annotations['diff'])][
             'conv_number'].interpolate(method='pad', limit_direction='forward')
-
+        annotations = annotations[['segment_onset', 'segment_offset', 'raw_filename', 'iti', 'delay', 'is_CT', 'conv_count']]
+        print(annotations)
         return annotations
     else:
         return pd.DataFrame([])
