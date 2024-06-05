f7c209d669020d1157d3b4349a6012da633850de
Loann Peurey <100950340+LoannPeurey@users.noreply.github.com>
2024-06-05 18:36:33 +0200

    Update conversations.py

--- ChildProject/pipelines/conversations.py
+++ ChildProject/pipelines/conversations.py
@@ -293,6 +293,10 @@ class Conversations(ABC):
 
         if matches.shape[0]:
             segments = self.am.get_segments(matches)
+            if not segments.shape[0]:
+                # no annotations for that unit
+                return pd.DataFrame(columns=([c.name for c in AnnotationManager.SEGMENTS_COLUMNS if c.required]
+                                         + list(annotations.columns) + ['conv_count']))
             segments = segments.dropna(subset='conv_count')
         else:
             # no annotations for that unit
