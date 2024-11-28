0e2f0c21da9921792dd808b32f1f12dc1f19d2b3
Loann Peurey <loannpeurey@gmail.com>
2024-11-28 17:43:30 +0100

    use str inside the subprocess call instead of Path as it can fail in pytohn<=3.7

--- ChildProject/pipelines/processors.py
+++ ChildProject/pipelines/processors.py
@@ -162,9 +162,9 @@ class BasicProcessor(AudioProcessor):
         if recording["recording_filename"] == "NA":
             return pd.DataFrame()
 
-        original_file = self.project.get_recording_path(
+        original_file = str(self.project.get_recording_path(
             recording["recording_filename"], self.input_profile
-        )
+        ))
         converted_filename = os.path.splitext(recording["recording_filename"])[0] + "." + self.format
 
         destination_file = os.path.join(
