773a282e2ddb230c73ed2c77817e64c5c4dba551
Loann Peurey <loannpeurey@gmail.com>
2025-07-21 17:23:43 +0200

    use uv for git-annex windows, rewrite instalaltions

--- install.ps1
+++ install.ps1
@@ -1,15 +1,6 @@
-# check if VERSION env variable is set, otherwise use "latest"
 $RELEASE_URL = "https://raw.githubusercontent.com/LAAC-LSCP/ChildProject/docs/rework_installation/env_windows.yml"
 
-curl.exe -L -o %TEMP%\env.yml $RELEASE_URL
-
-if (Get-Command git-annex) {
-    echo "Git annex found"
-} else {
-    echo "Git annex not found, installing it..."
-    curl.exe -L -o %TEMP%\git-annex-installer.exe https://downloads.kitenet.net/git-annex/windows/current/git-annex-installer.exe
-    %TEMP%\git-annex-installer.exe
-}
+curl.exe -o $env:TEMP\env.yml $RELEASE_URL
 
 if (Get-Command micromamba) {
     $ENV_MANAGER = "micromamba"
@@ -21,5 +12,17 @@ if (Get-Command micromamba) {
 }
 
 $success = "Installation Completed for environment childproject, activate it with: \n$ENV_MANAGER activate childproject"
-Invoke-Expression ("($ENV_MANAGER env create -f $env:TEMP\env.yml) -and (echo $SUCCESS)")
+# $install = Invoke-Expression ("($ENV_MANAGER env create -f $env:TEMP\env.yml) -and (echo $SUCCESS)")
+micromamba env create -f $env:TEMP\env.yml
+
+if($?)
+{
+   "Installing git-annex"
+   Invoke-Expression ("$ENV_MANAGER activate childproject")
+   uv tool install git-annex
+}
+else
+{
+   "Something went wrong in the environment creation"
+}
 
